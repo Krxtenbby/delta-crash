891bc209b19e4195c80ca9189f615fd132079272
Krxtenbby <125450594+Krxtenbby@users.noreply.github.com>
2024-08-03 12:45:09 +0100

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,9 @@
+//Ubah disini bro
+//SUBSCRIBE YT : cacaalvv
+global.baileys1 = require('@whiskeysockets/baileys') 
+global.prefa = ['','!','.',',','⚡','🤡']
+global.nocreator = ['62858227683239']
+global.owner = ['62858227683239']
+global.botname = 'Zeet'
+global.sticker1 = "セ Zeet"
+global.sticker2 = "セ Zeet"
